583c31fcb59bc8a241b1e75631f2e91615053de0
luzh <luzh16371@gmail.com>
2023-07-05 19:59:46 +0800

    [import] element-ui and demo.

--- packages/baklavajs-playground/src/main.ts
+++ packages/baklavajs-playground/src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//import App from "./App.vue";
+import App from "./App2.vue";
 
 import { BaklavaVuePlugin } from "../../baklavajs-plugin-renderer-vue/src";
 import "../../baklavajs-plugin-renderer-vue/src/styles/all.scss";
 import "./test.css";
 
 const app = createApp(App, {});
-app.use(BaklavaVuePlugin);
+app.use(ElementPlus);
 app.mount("#app");
--- packages/baklavajs-playground/vue.config.js
+++ packages/baklavajs-playground/vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   runtimeCompiler: true,
   publicPath: "./",
+  configureWebpack:{
+    module: {
+        rules: [
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: "javascript/auto"
+            },
+        ]
+    }
+  }
 }
+
